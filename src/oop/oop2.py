129834b414b69e3c78d7989cdd9e95ba75830f77
krikaz <pascal.bou@protonmail.com>
2019-10-04 09:32:12 +0200

    oop2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -26,8 +26,6 @@ class Motorcycle(GroundVehicle):
     def drive():
         return 'BRAAAP!!'
 
-# TODO
-
 
 vehicles = [
     GroundVehicle(),
@@ -39,4 +37,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
