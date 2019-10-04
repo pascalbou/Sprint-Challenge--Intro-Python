26964fc8bedbedaa861f725d772632b092029d41
krikaz <pascal.bou@protonmail.com>
2019-10-04 09:31:07 +0200

    oop1

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,12 +3,13 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
-
+    def drive():
+        return 'vroooom'
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -17,7 +18,16 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+
+    def drive():
+        return 'BRAAAP!!'
+
 # TODO
+
 
 vehicles = [
     GroundVehicle(),
